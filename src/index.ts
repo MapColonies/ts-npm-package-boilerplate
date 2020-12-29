fabbd2cd7e0fee116a8c8b74cc51bc67860e2dca
Daniel Hermon <danielhermonsync@gmail.com>
2020-12-29 13:54:01 +0200

    style: prettier

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export const cat = {
-  say: 'meow'
+  say: 'meow',
 };
--- tests/configurations/integration/jest.config.js
+++ tests/configurations/integration/jest.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    transform: {
-        '^.+\\.ts$': 'ts-jest',
-    },
-    coverageReporters: ['text', 'html'],
-    rootDir: '../../../.',
-    testMatch: ['<rootDir>/tests/integration/**/*.spec.ts'],
-    setupFiles: ['<rootDir>/tests/configurations/jest.setup.js'],
-    collectCoverage: true,
-    moduleDirectories: ['node_modules', 'src'],
-    collectCoverageFrom: ['<rootDir>/src/**/*.{ts}', '!**/node_modules/**', '!**/vendor/**'],
-    preset: 'ts-jest',
-    testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  coverageReporters: ['text', 'html'],
+  rootDir: '../../../.',
+  testMatch: ['<rootDir>/tests/integration/**/*.spec.ts'],
+  setupFiles: ['<rootDir>/tests/configurations/jest.setup.js'],
+  collectCoverage: true,
+  moduleDirectories: ['node_modules', 'src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts}', '!**/node_modules/**', '!**/vendor/**'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
 };

cf75a567f51824081771739d772384f1d7d7ef98
Daniel Hermon <danielhermonsync@gmail.com>
2020-12-29 13:50:50 +0200

    feat: added jest and updated npm scripts

---
+++ tests/configurations/unit/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    transform: {
+        '^.+\\.ts$': 'ts-jest',
+    },
+    testMatch: ['<rootDir>/tests/unit/**/*.spec.ts'],
+    coverageReporters: ['text', 'html'],
+    rootDir: '../../../.',
+    setupFiles: ['<rootDir>/tests/configurations/jest.setup.js'],
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+};

25e3a2e965aa0acd2fe4ab5b440c1432cf8ae6b5
Schnitz <12687466+CptSchnitz@users.noreply.github.com>
2026-02-26 10:26:39 +0200

    chore: more changes

--- tests/some-test.spec.ts
+++ tests/some-test.spec.ts
@@ -3,6 +3,7 @@ import { describe, it, expect } from 'vitest';
 describe('plus', function () {
   it('should equal 2 for 1 plus 1', function () {
     const expectedResult = 2;
+
     expect(1 + 1).toEqual(expectedResult);
   });
 });
